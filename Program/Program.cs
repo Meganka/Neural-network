b53b91a71ef6df88f368ded03edd07d54b9eadbc
Meganka <kaleta.mag@gmail.com>
2017-05-30 17:40:12 +0200

    Implementacja sieci neuronowej + prezentacja

---
+++ Program/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+//BIAI Project 
+    class Manager
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------- BIAI Project - Neural Network -----------------  ");
+            Console.WriteLine("Iris setosa = 0 0 1,\nIris versicolor = 0 1 0,\nIris virginica = 1 0 0,\n ");
+            Console.WriteLine("Data: sepal length, sepal width, petal length, petal width\n");
+            Console.WriteLine("For example: 5.4, 3.4, 1.7, 0.2 ");
+            Console.WriteLine("-------------------------------------------------------------------  ");
+           
+            double[][] data = null;
+            Tools.fileReader(ref data, @"D:\DANE.txt"); //reading data from file
+            double[][] dataForTest = null;
+            double[][] dataForTraining = null;
+            DataPrepare.makeTrainTest(data, out dataForTest, out dataForTraining);
+            DataPrepare.normalize(dataForTest, new int[] { 0, 1, 2, 3 });
+            DataPrepare.normalize(dataForTraining, new int[] { 0, 1, 2, 3 });
+
+            Console.WriteLine("I'm creating neural network...");
+            const int input = 4;
+            const int hidden = 7;
+            const int output = 3;
+            NeuralNetwork nn = new NeuralNetwork(input, hidden, output);
+            nn.initializeWeights(); //Initializing weights and bias to small random values
+
+            //TRAINING
+
+            //Settings for training
+            int epochos = 2000;
+            double learnRate = 0.05;
+            double momentum = 0.01;
+            double weightDecay = 0.0001;
+            Console.WriteLine("Start training");
+            DateTime startTime = DateTime.Now;
+            nn.training(dataForTest, epochos, learnRate, momentum, weightDecay); //mean squared error < 0.020 stopping condition
+            DateTime stopTime = DateTime.Now;
+            TimeSpan roznica = stopTime - startTime;
+            Console.WriteLine("Training time:" + roznica.TotalSeconds);
+            Console.WriteLine("Training complete! :)");
+            double[] weights = nn.getWeights();
+
+            //Console.WriteLine("nn weights and bias values:");
+             //Tools.showVector(weights, 10, 3, true);
+
+            //Accurancy
+            double accurancyTrain = nn.accuracy(dataForTest);
+            Console.WriteLine("---------------------Accurancy on Neural Network-------------------  ");
+            Console.WriteLine("\nAccuracy on training data = " + accurancyTrain.ToString("F2"));
+            double accurancyTest = nn.accuracy(dataForTraining);
+            Console.WriteLine("Accuracy on test data = " + accurancyTest.ToString("F2"));
+
+             //TESTING BY USER
+            double[] numbers = new double[4];
+            string ask;
+            Console.WriteLine("\n-------------------------------------------------------------------  ");
+            Console.WriteLine("Would you like to test ? [Y/N]");
+            ask = Console.ReadLine();
+                while (ask.ToUpper() == "Y")
+                     {
+                       Console.WriteLine("Input 4 values");
+                       for (int a = 0; a < 4; a++)
+                           {
+                               numbers[a] = Convert.ToDouble(Console.ReadLine(), System.Globalization.CultureInfo.InvariantCulture);
+                           }
+                       Console.WriteLine(Tools.outputName(ref nn, DataPrepare.normalizeInput(numbers,data)));    
+                       Console.WriteLine("Would u like to test one more time ? [Y/N]");
+                       ask = Console.ReadLine();
+                     }
+            Console.ReadLine();
+        } 
+} 
+
+    
---
+++ Program/Tools.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class Tools
+    {
+    public static string outputName(ref NeuralNetwork nn, double[] numbers)
+    {
+        string ret = "";
+        const int tot_len = 3;
+        double[] results = nn.forward(numbers);
+        for (int a = 0; a < tot_len; ++a)
+        {
+            if (results[a] + 0.15 > 1)
+                ret += "1";
+            else if (results[a] - 0.15 < 0)
+                ret += "0";
+             Console.WriteLine(results[a]); //u can print real output of nn 
+        }
+
+        if (ret == "001")
+            return "Iris Setosa";
+        else if (ret == "010")
+            return "Iris Versicolor";
+        else if (ret == "100")
+            return "Iris Virginica";
+        else
+        {
+            ret = "";
+            for (int a = 0; a < tot_len; ++a)
+            {
+                if (results[a] + 0.35 > 1)
+                    ret += "1";
+                else if (results[a] - 0.35 < 0)
+                    ret += "0";
+            }
+
+            if (ret == "001")
+                return "I am not quite sure i guess it is : Iris Setosa";
+            else if (ret == "010")
+                return "I am not quite sure i guess it is : Iris Versicolor";
+            else if (ret == "100")
+                return "I am not quite sure i guess it is : Iris Virginica";
+            else
+            {
+                return "I dont know what type of iris it is, sorry train me better";
+            }
+        }
+    }
+    public static void fileReader(ref double[][] allData, string path)
+        {
+
+            const int size_2d = 7;
+            string[] splitchar = new string[] { ", " };
+            string[] lines = System.IO.File.ReadAllLines(path);
+            string[][] splitlines = new string[lines.Length][];
+            allData = new double[lines.Length][];
+
+            for (int a = 0; a < lines.Length; ++a)
+            {
+                splitlines[a] = new string[size_2d];
+                allData[a] = new double[size_2d];
+            }
+            for (int a = 0; a < lines.Length; ++a)
+                splitlines[a] = lines[a].Split(splitchar, StringSplitOptions.None);
+
+            for (int a = 0; a < lines.Length; ++a)
+                for (int b = 0; b < size_2d; ++b)
+                    allData[a][b] = Convert.ToDouble(splitlines[a][b], System.Globalization.CultureInfo.InvariantCulture);
+
+        }
+
+       public static void showMatrix(double[][] matrix, int numRows, int decimals, bool newLine)
+        {
+            for (int i = 0; i < numRows; ++i)
+            {
+                Console.Write(i.ToString().PadLeft(3) + ": ");
+                for (int j = 0; j < matrix[i].Length; ++j)
+                {
+                    if (matrix[i][j] >= 0.0) Console.Write(" "); else Console.Write("-");
+                    Console.Write(Math.Abs(matrix[i][j]).ToString("F" + decimals) + " ");
+                }
+                Console.WriteLine("");
+            }
+            if (newLine == true) Console.WriteLine("");
+        }
+
+        public static void showVector(double[] vector, int valsPerRow, int decimals, bool newLine)
+        {
+            for (int i = 0; i < vector.Length; ++i)
+            {
+                if (i % valsPerRow == 0) Console.WriteLine("");
+                Console.Write(vector[i].ToString("F" + decimals).PadLeft(decimals + 4) + " ");
+            }
+            if (newLine == true) Console.WriteLine("");
+        }
+    }
+
